c5724e2c5c4cf993333132d1a5eb170c0b1e668b
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-11-28 10:43:11 +0530

    initial

---
+++ day1.js
@@ -0,0 +1,6 @@
+document.getElementById("head").innerHTML = "Hello from Script file !!"
+
+
+firstname= "john"
+lastname = "doe"
+document.write( `Welcome ${firstname},${lastname}`);
---
+++ day3.js
@@ -0,0 +1,21 @@
+let a = 6;
+let b = 9
+console.log(a+b)  // - * / %
+
+let x = 7;
+x += 90;
+console.log(x) //+= -= *= /= %=
+
+let  square = 9;
+console.log(square ** 2) // square
+console.log(square * 2) // double
+
+let c1 = 100;
+let c2 = 10
+let c3 = 90
+let c4 = 78
+c4--;
+console.log(c1 > c2 || c3 > c4)
+console.log(c4)
+
+document.getElementById("sum").innerText = c2;
